2e21c9d1e00dca903e1c0b0cc240d480e957af8b
Linna <karolimahh@gmail.com>
2024-10-07 08:58:08 -0300

    Desenvolvimento2

--- analisededados.py
+++ analisededados.py
@@ -1,18 +1,19 @@
-# Importando a biblioteca pandas
+# Importa bibliotecas
 import pandas as pd
+import matplotlib.pyplot as plt
 
-# Lendo o arquivo CSV
+# Le o arquivo CSV
 df = pd.read_csv('C:/Users/karol/Documents/Analise de Dados/turnover.csv', encoding='ISO-8859-1')
 
 
-# Exibindo as primeiras linhas do dataframe
+# Exibe as primeiras linhas do dataframe
 print(df.head())
 
-# Verificando informações gerais dos dados (tipo de dados, valores nulos, etc.)
+# Verifica informações gerais dos dados (tipo de dados, valores nulos, etc.)
 print(df.info())
 
 # Estatísticas descritivas das colunas numéricas
 print(df.describe())
 
-# Verificando se há valores nulos
+# Verifica se há valores nulos
 print(df.isnull().sum())
