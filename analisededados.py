49609ee2d7e58c87f461b3cb8bd8653264f59b2e
Linna <karolimahh@gmail.com>
2024-10-11 22:51:11 -0300

    Novo

--- analisededados.py
+++ analisededados.py
@@ -1,19 +1,19 @@
-# Importa bibliotecas
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Le o arquivo CSV
-df = pd.read_csv('C:/Users/karol/Documents/Analise de Dados/turnover.csv', encoding='ISO-8859-1')
+dados = pd.read_csv('C:/Users/karol/Documents/Analise de Dados/turnover.csv', sep= ",", encoding='ISO-8859-1') 
 
+colunas = ['gender', 'age', 'industry', 'profession', 'way', 'extraversion', 'independ', 'selfcontrol', 'anxiety','novator']  
+dados_filtrados = dados[colunas]
+print(dados_filtrados)
+#
+genero = dados_filtrados['gender'].value_counts()
 
-# Exibe as primeiras linhas do dataframe
-print(df.head())
-
-# Verifica informações gerais dos dados (tipo de dados, valores nulos, etc.)
-print(df.info())
-
-# Estatísticas descritivas das colunas numéricas
-print(df.describe())
-
-# Verifica se há valores nulos
-print(df.isnull().sum())
+#
+plt.figure(figsize=(10, 6))
+genero.plot(kind='bar', color='skyblue')
+plt.title('Distribuição de Gêneros')
+plt.xlabel('Gênero')
+plt.ylabel('Contagem')
+plt.xticks(rotation=0)
+plt.show()
